3681372dc431136f6d9920e64fced8d6d5732606
absar <abasar.a@protectt.ai>
2026-09-06 16:43:57 +0530

    feat: improve AOP logging for exceptions and execution time

--- product-service/src/main/java/com/ecommerce/product/aspect/ProductLoggingAspect.java
+++ product-service/src/main/java/com/ecommerce/product/aspect/ProductLoggingAspect.java
@@ -1,6 +1,8 @@
 package com.ecommerce.product.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
@@ -13,22 +15,20 @@ public class ProductLoggingAspect {
     private static final Logger log =LoggerFactory.getLogger(ProductLoggingAspect.class);
 
     @Around("@annotation(com.ecommerce.product.annotation.LogExecutionTime)")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint)
-            throws Throwable {
-
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long executionTime = System.currentTimeMillis() - startTime;
+            log.info("Method: {} executed in {} ms",joinPoint.getSignature().getName(),executionTime);
+        }
+    }
+    
+    
+    @AfterThrowing(pointcut = "execution(* com.ecommerce.product.service.*.*(..))",throwing = "exception")
+    public void logException(JoinPoint joinPoint,Exception exception) {
 
-        Object result = joinPoint.proceed();
-
-        long executionTime =
-                System.currentTimeMillis() - startTime;
-
-        log.info(
-                "Method: {} executed in {} ms",
-                joinPoint.getSignature().getName(),
-                executionTime
-        );
-
-        return result;
+        log.error("Exception in method: {} - {}",joinPoint.getSignature().getName(),exception.getMessage());
     }
 }
