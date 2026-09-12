3d01c001431719a2c8c57cd8716752b40d6a17db
absar <abasar.a@protectt.ai>
2026-09-12 20:06:11 +0530

    feat(security): add basic authentication and role-based authorization

--- product-service/src/main/java/com/ecommerce/product/config/SecurityConfig.java
+++ product-service/src/main/java/com/ecommerce/product/config/SecurityConfig.java
@@ -1,27 +1,54 @@
-package com.ecommerce.product.config;
+	package com.ecommerce.product.config;
+	
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.http.HttpMethod;
+	import org.springframework.security.config.Customizer;
+	import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+	import org.springframework.security.web.SecurityFilterChain;
+	import org.springframework.security.core.userdetails.User;
+	import org.springframework.security.core.userdetails.UserDetails;
+	import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+	import org.springframework.security.core.userdetails.UserDetailsService;
+	import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+	import org.springframework.security.crypto.password.PasswordEncoder;
+	
+	@Configuration
+	public class SecurityConfig {
+	
+		@Bean
+		SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		    http
+		        .csrf(csrf -> csrf.disable())
+		        .authorizeHttpRequests(auth -> auth
+		            .requestMatchers("/swagger-ui.html","/swagger-ui/**","/v3/api-docs/**").permitAll()
+		            .requestMatchers(HttpMethod.DELETE,"/api/v1/products/**").hasRole("ADMIN")
+		            .anyRequest().authenticated()
+		        )
+		        .httpBasic(Customizer.withDefaults());
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                		"/swagger-ui.html",
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**"
-                ).permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
-
-        return http.build();
-    }
-}
+		    return http.build();
+		}
+	    @Bean
+	    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+	
+	        UserDetails admin = User.builder()
+	                .username("admin")
+	                .password(passwordEncoder.encode("admin123"))
+	                .roles("ADMIN")
+	                .build();
+	
+	        UserDetails user = User.builder()
+	                .username("user")
+	                .password(passwordEncoder.encode("user123"))
+	                .roles("USER")
+	                .build();
+	
+	        return new InMemoryUserDetailsManager(admin, user);
+	    }
+	
+	    @Bean
+	    PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+	}
